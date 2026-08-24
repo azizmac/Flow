b3965600e84931ac97f2dd9dcf0886be6cad0cbf
Motorin Ilya <motorin.ilya.work@yandex.ru>
2026-08-24 22:59:52 +0500

    init: create structure of project

---
+++ tests/Flow.Application.Tests/PlaceholderTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Flow.Application.Tests;
+
+public class PlaceholderTests
+{
+    [Fact]
+    public void Placeholder()
+    {
+        Assert.True(true);
+    }
+}
---
+++ tests/Flow.Infrastructure.Tests/PlaceholderTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Flow.Infrastructure.Tests;
+
+public class PlaceholderTests
+{
+    [Fact]
+    public void Placeholder()
+    {
+        Assert.True(true);
+    }
+}
